198873376d5aa724587af6790bea9bead23b4464
melodicore <datafox@datafox.me>
2024-12-04 10:53:10 +0200

    Day 4 part 2

--- src/main/java/me/datafox/aoc2024/Day4.java
+++ src/main/java/me/datafox/aoc2024/Day4.java
@@ -16,6 +16,13 @@ public class Day4 {
         return countXMAS(map);
     }
 
+    public static int solve2(URL url) {
+        char[][] map = FileUtils.linesAsStream(url)
+                .map(String::toCharArray)
+                .toArray(char[][]::new);
+        return countMAS(map);
+    }
+
     private static int countXMAS(char[][] map) {
         assert map.length == map[0].length;
         int count = 0;
@@ -80,7 +87,49 @@ public class Day4 {
         return count;
     }
 
-    public static int solve2(URL url) {
-        return 0;
+    private static int countMAS(char[][] map) {
+        assert map.length == map[0].length;
+        int count = 0;
+        for(int i = 1; i < map.length - 1; i++) {
+            for(int j = 1; j < map.length - 1; j++) {
+                if(isMAS(map, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static boolean isMAS(char[][] map, int i, int j) {
+        if(map[i][j] != 'A') {
+            return false;
+        }
+        char c1 = map[i - 1][j - 1];
+        char c2 = map[i + 1][j + 1];
+        char c3 = map[i - 1][j + 1];
+        char c4 = map[i + 1][j - 1];
+        if(c1 == 'M') {
+            if(c2 != 'S') {
+                return false;
+            }
+        } else if(c1 == 'S') {
+            if(c2 != 'M') {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        if(c3 == 'M') {
+            if(c4 != 'S') {
+                return false;
+            }
+        } else if(c3 == 'S') {
+            if(c4 != 'M') {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        return true;
     }
 }
