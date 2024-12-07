48e1ac1ff6b4a85554823e00173193a53bc5ec04
melodicore <datafox@datafox.me>
2024-12-07 11:24:21 +0200

    Day 7 part 2

--- src/main/java/me/datafox/aoc2024/Day7.java
+++ src/main/java/me/datafox/aoc2024/Day7.java
@@ -16,7 +16,9 @@ public class Day7 {
     }
 
     public static long solve2(URL url) {
-        return 0;
+        return FileUtils.linesAsStream(url)
+                .mapToLong(Day7::solveEquationWithConcat)
+                .sum();
     }
 
     private static long solveEquation(String str) {
@@ -36,6 +38,40 @@ public class Day7 {
                     l += params[j + 1];
                 } else {
                     l *= params[j + 1];
+                }
+            }
+            if(l == answer) {
+                return answer;
+            }
+        }
+        return 0;
+    }
+
+    private static long solveEquationWithConcat(String str) {
+        String[] split = str.split(": ", 2);
+        long answer = Long.parseLong(split[0]);
+        long[] params = Arrays.stream(split[1].split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        int iterations = 3;
+        for(int i = 0; i < params.length - 2; i++) {
+            iterations *= 3;
+        }
+        for(int i = 0; i < iterations; i++) {
+            long l = params[0];
+            String b3 = Integer.toString(i, 3);
+            for(int j = 0; j < params.length - 1; j++) {
+                int k;
+                if(b3.length() <= j) {
+                    k = 0;
+                } else {
+                    k = Integer.parseInt(Character.toString(b3.charAt(b3.length() - j - 1)));
+                }
+                switch(k) {
+                    case 0 -> l += params[j + 1];
+                    case 1 -> l *= params[j + 1];
+                    case 2 -> l = Long.parseLong(Long.toString(l) + params[j + 1]);
+                    default -> throw new RuntimeException();
                 }
             }
             if(l == answer) {
