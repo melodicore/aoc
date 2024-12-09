744d77f9adf56530e08b6af5db035619c8eba629
melodicore <datafox@datafox.me>
2024-12-09 15:48:29 +0200

    Day 5 part 2 (2023)

--- src/main/java/me/datafox/aoc/aoc2023/Day5.java
+++ src/main/java/me/datafox/aoc/aoc2023/Day5.java
@@ -3,7 +3,9 @@ package me.datafox.aoc.aoc2023;
 import me.datafox.aoc.FileUtils;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.LongStream;
 
 /**
@@ -31,7 +33,22 @@ public class Day5 {
     }
 
     public static long solve2(URL url) {
-        return 0;
+        String[] maps = FileUtils.string(url).split("seeds: |\\n\\n.+:\\n");
+        long[] seedInputs = Arrays.stream(maps[1].split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        SeedRange[] seeds = getSeedRanges(seedInputs);
+        Range[][] ranges = new Range[maps.length - 2][];
+        for(int i = 0; i < maps.length - 2; i++) {
+            String str = maps[i + 2];
+            ranges[i] = str.lines()
+                    .map(Range::parse)
+                    .toArray(Range[]::new);
+        }
+        return Arrays.stream(calculatePositions(seeds, ranges))
+                .mapToLong(SeedRange::start)
+                .min()
+                .orElse(0);
     }
 
     private static long calculatePosition(long seed, Range[][] ranges) {
@@ -45,6 +62,50 @@ public class Day5 {
             }
         }
         return seed;
+    }
+
+    private static SeedRange[] getSeedRanges(long[] seeds) {
+        SeedRange[] ranges = new SeedRange[seeds.length / 2];
+        for(int i = 0; i < ranges.length; i++) {
+            ranges[i] = new SeedRange(seeds[i * 2], seeds[i * 2 + 1]);
+        }
+        return ranges;
+    }
+
+    private static SeedRange[] calculatePositions(SeedRange[] seeds, Range[][] ranges) {
+        for(Range[] arr : ranges) {
+            List<SeedRange> completed = new ArrayList<>();
+            List<SeedRange> list = new ArrayList<>();
+            for(Range range : arr) {
+                list.clear();
+                for(SeedRange seed : seeds) {
+                    SeedRange[] split = seed.split(range);
+                    if(split.length == 1) {
+                        if(seed.start() >= range.source &&
+                                seed.start() + seed.range() <= range.source() + range.range()) {
+                            completed.add(seed.convert(range));
+                        } else {
+                            list.add(seed);
+                        }
+                    }
+                    else if(split.length == 3) {
+                        list.add(split[0]);
+                        completed.add(split[1].convert(range));
+                        list.add(split[2]);
+                    } else if(range.convert(seed.start()) == seed.start()) {
+                        list.add(split[0]);
+                        completed.add(split[1].convert(range));
+                    } else {
+                        completed.add(split[0].convert(range));
+                        list.add(split[1]);
+                    }
+                }
+                seeds = list.toArray(SeedRange[]::new);
+            }
+            completed.addAll(list);
+            seeds = completed.toArray(SeedRange[]::new);
+        }
+        return seeds;
     }
 
     private record Range(long dest, long source, long range) {
@@ -61,6 +122,42 @@ public class Day5 {
                 return dest + (input - source);
             }
             return input;
+        }
+    }
+
+    private record SeedRange(long start, long range) {
+        public SeedRange[] split(Range r) {
+            if(start < r.source) {
+                if(start + range <= r.source) {
+                    return new SeedRange[] { this };
+                } else if(start + range < r.source + r.range) {
+                    return split(r.source);
+                } else {
+                    SeedRange[] split1 = split(r.source);
+                    SeedRange[] split2 = split1[1].split(r.source + r.range);
+                    return new SeedRange[] { split1[0], split2[0], split2[1] };
+                }
+            } else {
+                if(start >= r.source + r.range) {
+                    return new SeedRange[] { this };
+                } else if(start + range <= r.source + r.range) {
+                    return new SeedRange[] { this };
+                } else {
+                    return split(r.source + r.range);
+                }
+            }
+        }
+
+        public SeedRange convert(Range r) {
+            return new SeedRange(r.convert(start), range);
+        }
+
+        private SeedRange[] split(long pos) {
+            assert pos > start;
+            return new SeedRange[] {
+                    new SeedRange(start, pos - start),
+                    new SeedRange(pos, range - (pos - start))
+            };
         }
     }
 }
