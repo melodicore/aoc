bb94986030dc6c9d369a23caf5ff6c47c5c54b68
melodicore <datafox@datafox.me>
2024-12-09 12:16:39 +0200

    Day 1 part 2 (2023)

--- src/main/java/me/datafox/aoc/aoc2023/Day1.java
+++ src/main/java/me/datafox/aoc/aoc2023/Day1.java
@@ -4,6 +4,7 @@ import me.datafox.aoc.FileUtils;
 import me.datafox.aoc.StringUtils;
 
 import java.net.URL;
+import java.util.List;
 
 /**
  * Advent of Code 2023 day 1 solutions.
@@ -11,6 +12,9 @@ import java.net.URL;
  * @author datafox
  */
 public class Day1 {
+    private static final List<String> NUMBERS = List.of(
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine");
+
     public static int solve1(URL url) {
         return FileUtils.linesAsStream(url)
                 .mapToInt(Day1::getNumber)
@@ -18,7 +22,25 @@ public class Day1 {
     }
 
     public static int solve2(URL url) {
-        return 0;
+        return FileUtils.linesAsStream(url)
+                .map(Day1::numberToDigit)
+                .mapToInt(Day1::getNumber)
+                .sum();
+    }
+
+    private static String numberToDigit(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            int temp = i;
+            NUMBERS.stream()
+                    .filter(n -> s.startsWith(n, temp))
+                    .findFirst()
+                    .map(NUMBERS::indexOf)
+                    .map(n -> n + 1)
+                    .ifPresent(sb::append);
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 
     private static int getNumber(String str) {
