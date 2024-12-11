4369d6b43193185bcdfbb8d18b304db8ab816e98
melodicore <datafox@datafox.me>
2024-12-11 12:33:22 +0200

    Day 11 part 2 (2024)

--- src/main/java/me/datafox/aoc/aoc2024/Day11.java
+++ src/main/java/me/datafox/aoc/aoc2024/Day11.java
@@ -4,6 +4,10 @@ import me.datafox.aoc.FileUtils;
 
 import java.net.URL;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
 /**
@@ -22,7 +26,31 @@ public class Day11 {
     }
 
     public static long solve2(URL url) {
-        return 0;
+        Map<Long,Long> rocks = new LinkedHashMap<>(Arrays
+                .stream(FileUtils.string(url).split(" "))
+                .map(Long::parseLong)
+                .collect(Collectors.toMap(Function.identity(), l -> 1L)));
+        for(int i = 0; i < 75; i++) {
+            Map<Long,Long> copy = new LinkedHashMap<>(rocks);
+            rocks.clear();
+            for(Map.Entry<Long,Long> e : copy.entrySet()) {
+                if(e.getKey() == 0) {
+                    rocks.put(1L, rocks.getOrDefault(1L, 0L) + e.getValue());
+                } else {
+                    String s = Long.toString(e.getKey());
+                    if((s.length() & 1) == 0) {
+                        Long l = Long.parseLong(s.substring(0, s.length() / 2));
+                        rocks.put(l, rocks.getOrDefault(l, 0L) + e.getValue());
+                        l = Long.parseLong(s.substring(s.length() / 2));
+                        rocks.put(l, rocks.getOrDefault(l, 0L) + e.getValue());
+                    } else {
+                        Long l = e.getKey() * 2024;
+                        rocks.put(l, rocks.getOrDefault(l, 0L) + e.getValue());
+                    }
+                }
+            }
+        }
+        return rocks.values().stream().mapToLong(Long::longValue).sum();
     }
 
     private static LongStream blink(long l) {
