2e99457c74ed83279d68ca42d8d5026af518786e
melodicore <datafox@datafox.me>
2024-12-13 17:52:09 +0200

    Day 13 part 2 (2024)

--- src/main/java/me/datafox/aoc/aoc2024/Day13.java
+++ src/main/java/me/datafox/aoc/aoc2024/Day13.java
@@ -3,7 +3,7 @@ package me.datafox.aoc.aoc2024;
 import me.datafox.aoc.FileUtils;
 
 import java.net.URL;
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Advent of Code 2024 day 11 solutions.
@@ -18,8 +18,11 @@ public class Day13 {
                 .sum();
     }
 
-    public static int solve2(URL url) {
-        return 0;
+    public static long solve2(URL url) {
+        return Arrays.stream(FileUtils.string(url).split("\n\n"))
+                .map(Day13::parseLongMachine)
+                .mapToLong(LongMachine::minPriceForPrize)
+                .sum();
     }
 
     private static Machine parseMachine(String str) {
@@ -32,6 +35,23 @@ public class Day13 {
         String p = split[2].split("Prize: X=", 2)[1];
         String[] pp = p.split(", Y=", 2);
         return new Machine(Integer.parseInt(ap[0]),
+                Integer.parseInt(ap[1]),
+                Integer.parseInt(bp[0]),
+                Integer.parseInt(bp[1]),
+                Integer.parseInt(pp[0]),
+                Integer.parseInt(pp[1]));
+    }
+
+    private static LongMachine parseLongMachine(String str) {
+        String[] split = str.split("\n");
+        assert split.length == 3;
+        String ba = split[0].split("Button A: X\\+", 2)[1];
+        String[] ap = ba.split(", Y\\+", 2);
+        String bb = split[1].split("Button B: X\\+", 2)[1];
+        String[] bp = bb.split(", Y\\+", 2);
+        String p = split[2].split("Prize: X=", 2)[1];
+        String[] pp = p.split(", Y=", 2);
+        return new LongMachine(Integer.parseInt(ap[0]),
                 Integer.parseInt(ap[1]),
                 Integer.parseInt(bp[0]),
                 Integer.parseInt(bp[1]),
@@ -63,6 +83,23 @@ public class Day13 {
 
         private int maxBForA(int a) {
             return Math.min((px - a * ax) / bx, (py - a * ay) / by);
+        }
+    }
+
+    private record LongMachine(long ac, long ax, long ay, long bc, long bx, long by, long px, long py) {
+        public LongMachine(long ax, long ay, long bx, long by, long px, long py) {
+            this(3, ax, ay, 1, bx, by, px + 10000000000000L, py + 10000000000000L);
+        }
+
+        public long minPriceForPrize() {
+            long determinant = ax * by - bx * ay;
+            long a = (px * by - py * bx) / determinant;
+            long b = (py * ax - px * ay) / determinant;
+            if(a * ax + b * bx == px && a * ay + b * by == py) {
+                return a * 3 + b;
+            } else {
+                return 0;
+            }
         }
     }
 }
