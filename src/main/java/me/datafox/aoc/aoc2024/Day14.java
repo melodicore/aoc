3c9c4dab1e8349b49372c4006fcdd2ef9c7d764c
melodicore <datafox@datafox.me>
2024-12-14 11:33:24 +0200

    Day 14 part 2 (2024)

--- src/main/java/me/datafox/aoc/aoc2024/Day14.java
+++ src/main/java/me/datafox/aoc/aoc2024/Day14.java
@@ -3,6 +3,13 @@ package me.datafox.aoc.aoc2024;
 import me.datafox.aoc.Coordinate;
 import me.datafox.aoc.FileUtils;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.List;
@@ -25,6 +32,70 @@ public class Day14 {
                 .map(r -> r.wrap(WIDTH, HEIGHT))
                 .collect(Collectors.toSet());
         return splitQuadrants(robots, WIDTH, HEIGHT).stream().mapToInt(Set::size).reduce(1, (i, j) -> i * j);
+    }
+
+    public static int solve2(URL url) {
+        Set<Robot> robots = FileUtils.linesAsStream(url)
+                .map(Robot::parse)
+                .collect(Collectors.toSet());
+        Lock lock = new Lock();
+        JFrame frame = new JFrame();
+        BufferedImage image = drawRobots(robots);
+        JLabel label = new JLabel(new ImageIcon(image));
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+        frame.setLocationRelativeTo(null);
+        frame.pack();
+        frame.addKeyListener(new Listener(lock));
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                synchronized(lock) {
+                    lock.action = Action.EXIT;
+                    lock.notifyAll();
+                }
+                super.windowClosed(e);
+            }
+        });
+        frame.setVisible(true);
+        int totalSteps = 0;
+        loop: while(true) {
+            synchronized(lock) {
+                try {
+                    lock.wait();
+                    int steps;
+                    switch(lock.action) {
+                        case NEXT -> steps = 1;
+                        case PREVIOUS -> steps = -1;
+                        case NEXT_10 -> steps = 10;
+                        case PREVIOUS_10 -> steps = -10;
+                        case NEXT_101 -> steps = 101;
+                        case EXIT -> {
+                            break loop;
+                        }
+                        default -> steps = 0;
+                    }
+                    if(steps != 0) {
+                        totalSteps += steps;
+                        robots = robots.stream()
+                                .map(r -> r.move(steps))
+                                .map(r -> r.wrap(WIDTH, HEIGHT))
+                                .collect(Collectors.toSet());
+                        image = drawRobots(robots);
+                        label.setIcon(new ImageIcon(image));
+                    }
+                } catch(InterruptedException ignored) {}
+            }
+        }
+        frame.dispose();
+        return totalSteps;
+    }
+
+    private static BufferedImage drawRobots(Set<Robot> robots) {
+        BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        for(Robot r : robots) {
+            image.setRGB(r.coord().x(), r.coord().y(), 0xFFFFFF);
+        }
+        return image;
     }
 
     private static List<Set<Robot>> splitQuadrants(Set<Robot> robots, int width, int height) {
@@ -55,10 +126,6 @@ public class Day14 {
         return List.of(ul, ur, dl, dr);
     }
 
-    public static int solve2(URL url) {
-        return 0;
-    }
-
     private record Robot(Coordinate coord, Coordinate speed) {
         public static Robot parse(String str) {
             String[] split = str.substring(2).split(",| v=", 4);
@@ -74,5 +141,70 @@ public class Day14 {
         public Robot wrap(int width, int height) {
             return new Robot(coord.modulo(width, height), speed);
         }
+    }
+
+    private static class Listener implements KeyListener {
+        private final Lock lock;
+
+        public Listener(Lock lock) {
+            this.lock = lock;
+        }
+
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            synchronized(lock) {
+                switch(e.getKeyCode()) {
+                    case KeyEvent.VK_RIGHT -> {
+                        lock.action = Action.NEXT;
+                        lock.notifyAll();
+                    }
+                    case KeyEvent.VK_LEFT -> {
+                        lock.action = Action.PREVIOUS;
+                        lock.notifyAll();
+                    }
+                    case KeyEvent.VK_UP -> {
+                        lock.action = Action.NEXT_10;
+                        lock.notifyAll();
+                    }
+                    case KeyEvent.VK_DOWN -> {
+                        lock.action = Action.PREVIOUS_10;
+                        lock.notifyAll();
+                    }
+                    case KeyEvent.VK_SPACE -> {
+                        lock.action = Action.NEXT_101;
+                        lock.notifyAll();
+                    }
+                    case KeyEvent.VK_ESCAPE -> {
+                        lock.action = Action.EXIT;
+                        lock.notifyAll();
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+
+
+    }
+
+    private static class Lock {
+        public Action action = Action.NEXT;
+    }
+
+    private enum Action {
+        NEXT,
+        PREVIOUS,
+        NEXT_10,
+        PREVIOUS_10,
+        NEXT_101,
+        EXIT
     }
 }
