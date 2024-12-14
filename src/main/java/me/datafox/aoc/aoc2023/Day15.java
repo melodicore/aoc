433b847d403c2a3a1628c5475df54498b38ceda7
melodicore <datafox@datafox.me>
2024-12-14 22:33:56 +0200

    Day 15 part 2 (2023)

--- src/main/java/me/datafox/aoc/aoc2023/Day15.java
+++ src/main/java/me/datafox/aoc/aoc2023/Day15.java
@@ -18,7 +18,65 @@ public class Day15 {
     }
 
     public static int solve2(URL url) {
-        return 0;
+        Node[] nodes = new Node[256];
+        for(String s : FileUtils.string(url).split(",")) {
+            String[] split = s.split("[-=]", 2);
+            int hash = hash(split[0]);
+            if(split[1].isEmpty()) {
+                if(nodes[hash] != null) {
+                    Node node = nodes[hash];
+                    if(node.key.equals(split[0])) {
+                        nodes[hash] = node.next;
+                    } else {
+                        Node parent = node;
+                        while(!node.key.equals(split[0]) && node.next != null) {
+                            parent = node;
+                            node = node.next;
+                        }
+                        if(node.key.equals(split[0])) {
+                            parent.next = node.next;
+                        }
+                    }
+                }
+            } else {
+                Node node = null;
+                Node parent = nodes[hash];
+                do {
+                    if(parent == null || parent.key.equals(split[0])) {
+                        node = parent;
+                        break;
+                    }
+                    if(parent.next == null) {
+                        break;
+                    }
+                    parent = parent.next;
+                } while(true);
+                if(node == null) {
+                    node = new Node();
+                    node.key = split[0];
+                    if(parent == null) {
+                        nodes[hash] = node;
+                    } else {
+                        parent.next = node;
+                    }
+                }
+                node.value = Integer.parseInt(split[1]);
+            }
+        }
+        int sum = 0;
+        for(int i = 0; i < nodes.length; i++) {
+            if(nodes[i] == null) {
+                continue;
+            }
+            int j = 1;
+            Node node = nodes[i];
+            do {
+                sum += (i + 1) * j * node.value;
+                node = node.next;
+                j++;
+            } while(node != null);
+        }
+        return sum;
     }
 
     private static int hash(String s) {
@@ -29,5 +87,13 @@ public class Day15 {
             hash %= 256;
         }
         return hash;
+    }
+
+    private static class Node {
+        public String key;
+
+        public int value;
+
+        public Node next;
     }
 }
