ed0bb0611b785605a3cd256c8f4b362aee1fc670
melodicore <datafox@datafox.me>
2024-12-20 19:09:34 +0200

    Day 18 part 2 (2023)

--- src/main/java/me/datafox/aoc/aoc2023/Day18.java
+++ src/main/java/me/datafox/aoc/aoc2023/Day18.java
@@ -17,21 +17,55 @@ import java.util.stream.Collectors;
 public class Day18 {
     private static final Set<Direction> UD = Set.of(Direction.UP, Direction.DOWN);
 
+    private static final Map<Set<Direction>,Set<Direction>> DIRS;
+
+    static {
+        DIRS = new HashMap<>();
+        for(Direction dir1 : Direction.values()) {
+            for(Direction dir2 : Direction.values()) {
+                if(dir1.equals(dir2)) {
+                    continue;
+                }
+                Set<Direction> set = EnumSet.of(dir1, dir2);
+                DIRS.put(set, set);
+            }
+        }
+    }
+
     private static int minX = 0, minY = 0, maxX = 0, maxY = 0;
+
+    private static long hor = 0;
 
     public static int solve1(URL url) {
         List<Trench> trenches = parseTrenches(FileUtils
                 .linesAsStream(url)
                 .map(s -> s.split(" "))
-                .toList());
+                .toList(), false);
         return calculateSize(trenches);
     }
 
-    public static int solve2(URL url) {
-        return 0;
+    public static long solve2(URL url) {
+        List<Trench> trenches = parseTrenches(FileUtils
+                .linesAsStream(url)
+                .map(s -> s.split(" ")[2])
+                .map(Day18::hexToInput)
+                .toList(), true);
+        return calculateLargeSize(trenches);
     }
 
-    private static List<Trench> parseTrenches(List<String[]> rules) {
+    private static String[] hexToInput(String s) {
+        String dir = switch(s.charAt(s.length() - 2)) {
+            case '0' -> "R";
+            case '1' -> "D";
+            case '2' -> "L";
+            case '3' -> "U";
+            default -> null;
+        };
+        String count = Integer.toString(Integer.parseInt(s.substring(2, s.length() - 2), 16));
+        return new String[] { dir, count, "" };
+    }
+
+    private static List<Trench> parseTrenches(List<String[]> rules, boolean skipHor) {
         Coordinate first = new Coordinate(0, 0);
         Coordinate last = first;
         List<Trench> trenches = new ArrayList<>();
@@ -42,6 +76,10 @@ public class Day18 {
             assert dir != null;
             for(int j = 0; j < Integer.parseInt(split[1]) - 1; j++) {
                 last = last.move(dir, 1);
+                if(skipHor && i != 0 && !UD.contains(dir)) {
+                    hor++;
+                    continue;
+                }
                 trenches.add(new Trench(last, Set.of(dir, dir.opposite())));
             }
             if(i + 1 < rules.size()) {
@@ -121,14 +159,69 @@ public class Day18 {
         return count + trenches.size();
     }
 
-    private static boolean isOpposite(Set<Direction> corner, Set<Direction> other) {
-        return corner.stream().noneMatch(other::contains);
-    }
-
     private static boolean isCorner(Set<Direction> dirs) {
         assert dirs.size() == 2;
         return !dirs.contains(dirs.iterator().next().opposite());
     }
 
-    private record Trench(Coordinate coord, Set<Direction> dirs) {}
+    private static long calculateLargeSize(List<Trench> trenches) {
+        Map<Integer,List<Trench>> map = new TreeMap<>();
+        for(Trench trench : trenches) {
+            if(!UD.equals(trench.dirs()) && !isCorner(trench.dirs())) {
+                continue;
+            }
+            if(!map.containsKey(trench.coord().y())) {
+                map.put(trench.coord().y(), new ArrayList<>());
+            }
+            map.get(trench.coord().y()).add(trench);
+        }
+        long count = 0;
+        for(List<Trench> list : map.values()) {
+            list.sort(Comparator.comparingInt(t -> t.coord().x()));
+            Trench last = null;
+            boolean inside = false;
+            for(int i = 0; i < list.size(); i++) {
+                Trench trench = list.get(i);
+                if(inside) {
+                    assert !trench.dirs().contains(Direction.LEFT);
+                    count += trench.coord().x() - last.coord().x() - 1;
+                    if(trench.dirs().contains(Direction.RIGHT)) {
+                        i++;
+                        Trench next = list.get(i);
+                        assert next.dirs().contains(Direction.LEFT);
+                        if(trench.dirs().contains(Direction.UP) != next.dirs().contains(Direction.UP)) {
+                            inside = false;
+                        }
+                        last = next;
+                        continue;
+                    } else {
+                        inside = false;
+                    }
+                } else {
+                    assert !trench.dirs().contains(Direction.LEFT);
+                    if(trench.dirs().contains(Direction.RIGHT)) {
+                        i++;
+                        Trench next = list.get(i);
+                        assert next.dirs().contains(Direction.LEFT);
+                        if(trench.dirs().contains(Direction.UP) != next.dirs().contains(Direction.UP)) {
+                            inside = true;
+                        }
+                        last = next;
+                        continue;
+                    } else {
+                        inside = true;
+                    }
+                }
+                last = trench;
+            }
+        }
+        return count + trenches.size() + hor;
+    }
+
+    private record Trench(Coordinate coord, Set<Direction> dirs) {
+        public Trench(Coordinate coord, Set<Direction> dirs) {
+            this.coord = coord;
+            this.dirs = DIRS.getOrDefault(dirs, dirs);
+        }
+    }
 }
